b5630529f15ddeaf58550ca9743fcabaad69683b
Sijan Malla <sijan.malla101@gmail.com>
2021-10-30 13:32:03 -0500

    Multiple ResultSet Error for unkown column

--- src/main/java/com/example/Company.java
+++ src/main/java/com/example/Company.java
@@ -12,20 +12,23 @@ public class Company {
         if (!con.isClosed()) {
             System.out.println("Project Executive Summary");
             Statement stmt = con.createStatement();
+            Statement stmts = con.createStatement();
             String query = "SELECT Dname, CONCAT(Fname,' ',Minit,'. ',Lname) As MGR, Pname FROM DEPARTMENT,EMPLOYEE , PROJECT WHERE EMPLOYEE.Ssn = DEPARTMENT.Mgr_ssn AND PROJECT.Dnum = DEPARTMENT.Dnumber ORDER BY Dname, Pname;";
             ResultSet rs = stmt.executeQuery(query);
 
             ArrayList<String> Dept = new ArrayList<>();
             while (rs.next()) {
                 String DeptName = rs.getString("Dname");
-                if (!Dept.contains(DeptName)) {
-                    ResultSet empInfo = stmt.executeQuery("SELECT COUNT(DISTINCT Fname), SUM(Hours), Pname FROM EMPLOYEE, PROJECT, WORKS_ON WHERE Pname = 'ProductZ' AND Ssn = Essn AND Pnumber=Pno;");
-                    System.out.println(rs.getString("Dname") + " - " + rs.getString("MGR") + empInfo.getString("Pname"));
-                    System.out.println(rs.getString("Pname"));
+                ResultSet empInfo = stmts.executeQuery("SELECT COUNT(DISTINCT Fname) AS num, SUM(Hours) AS hrs, Pname FROM EMPLOYEE, PROJECT, WORKS_ON, DEPARTMENT WHERE Ssn = Essn AND Pnumber=Pno AND Dnumber = Dnum GROUP BY Pname ORDER BY Dname,Pname;");
+                empInfo.next();
+                if (!Dept.contains(DeptName)) {                   
+                    System.out.println(rs.getString("Dname") + " - " + rs.getString("MGR"));
+                    System.out.println(rs.getString("Pname" +" "+empInfo.getInt("num")));
                     Dept.add(DeptName);
 
                 } else {
-                    System.out.println(rs.getString("Pname"));
+                    
+                    System.out.println(rs.getString("Pname")+" "+empInfo.getInt("num"));
                 }
 
             }
