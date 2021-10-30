cd708f2806cad689f96bb8b7d8f1810e77821cdd
Sijan Malla <sijan.malla101@gmail.com>
2021-10-30 11:52:27 -0500

    working on queries for report

---
+++ src/main/java/com/example/Company.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Company {
+    public static void main(String[] args) throws Exception {
+        Connection con = DBConnection.getConnection();
+
+        if (!con.isClosed()) {
+            System.out.println("Project Executive Summary");
+            Statement stmt = con.createStatement();
+            String query = "SELECT Dname, CONCAT(Fname,' ',Minit,'. ',Lname) As MGR, Pname FROM DEPARTMENT,EMPLOYEE , PROJECT WHERE EMPLOYEE.Ssn = DEPARTMENT.Mgr_ssn AND PROJECT.Dnum = DEPARTMENT.Dnumber ORDER BY Dname, Pname;";
+            ResultSet rs = stmt.executeQuery(query);
+
+            ArrayList<String> Dept = new ArrayList<>();
+            while (rs.next()) {
+                String DeptName = rs.getString("Dname");
+                if (!Dept.contains(DeptName)) {
+                    System.out.println(rs.getString("Dname") + " - " + rs.getString("MGR"));
+                    System.out.println(rs.getString("Pname"));
+                    Dept.add(DeptName);
+
+                } else {
+                    System.out.println(rs.getString("Pname"));
+                }
+
+            }
+        }
+    }
+
+}
