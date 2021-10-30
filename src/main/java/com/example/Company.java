8fee2189506e1586bae6a83465f32be1e4b5f983
Sijan Malla <sijan.malla101@gmail.com>
2021-10-30 14:41:08 -0500

    Page 1 report done. Null Checked. Formatting Done. Need to merge branch. This branch served its purpose.

--- src/main/java/com/example/Company.java
+++ src/main/java/com/example/Company.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.text.DecimalFormat;
 
 public class Company {
     public static void main(String[] args) throws Exception {
@@ -18,8 +19,9 @@ public class Company {
 
             ArrayList<String> Dept = new ArrayList<>();
 
-            ResultSet empInfo = stmts.executeQuery("SELECT COUNT(DISTINCT Fname) AS num, SUM(Hours) AS hrs, Pname FROM EMPLOYEE, PROJECT, WORKS_ON, DEPARTMENT WHERE Ssn = Essn AND Pnumber=Pno AND Dnumber = Dnum GROUP BY Pname ORDER BY Dname,Pname;");
+            ResultSet empInfo = stmts.executeQuery("SELECT COUNT(DISTINCT Fname) AS num, SUM(Hours) AS hrs,SUM(Salary*(Hours/40)) AS FC, Pname FROM EMPLOYEE, PROJECT, WORKS_ON, DEPARTMENT WHERE Ssn = Essn AND Pnumber=Pno AND Dnumber = Dnum AND Hours IS NOT NULL GROUP BY Pname ORDER BY Dname,Pname;");
 
+            DecimalFormat formatter = new DecimalFormat("#,###.00");
             while (rs.next() && empInfo.next()) {
                 String DeptName = rs.getString("Dname");
               
@@ -27,12 +29,12 @@ public class Company {
                 if (!Dept.contains(DeptName)) {                
                     System.out.println(rs.getString("Dname") + " - " + rs.getString("MGR"));
                     System.out.printf("%-24s",rs.getString("Pname") +" ");
-                    System.out.printf("%10d %15.1f\n",empInfo.getInt("num"),empInfo.getDouble("hrs"));
+                    System.out.printf("%10d %15.1f    $ %s\n",empInfo.getInt("num"),empInfo.getDouble("hrs"),formatter.format(empInfo.getDouble("FC")));
                     Dept.add(DeptName);
 
                 } else {
                     System.out.printf("%-24s",rs.getString("Pname")+" ");
-                    System.out.printf("%10d %15.1f\n",empInfo.getInt("num"),empInfo.getDouble("hrs"));
+                    System.out.printf("%10d %15.1f    $ %s\n",empInfo.getInt("num"),empInfo.getDouble("hrs"),formatter.format(empInfo.getDouble("FC")));
                 }
 
             }
--- src/main/java/com/example/For getting Hours and total Emp Number.sql
+++ src/main/java/com/example/For getting Hours and total Emp Number.sql
@@ -1,5 +1,5 @@
-SELECT Pname, COUNT(DISTINCT Fname) AS num, SUM(Hours) AS hrs
+SELECT Pname, COUNT(DISTINCT Fname) AS num, SUM(Hours) AS hrs, SUM(Salary*(Hours/40))
 FROM EMPLOYEE, PROJECT, WORKS_ON, DEPARTMENT 
-WHERE Ssn = Essn AND Pnumber=Pno AND Dnumber = Dnum 
+WHERE Ssn = Essn AND Pnumber=Pno AND Dnumber = Dnum AND Hours IS NOT NULL
 GROUP BY Pname 
 ORDER BY Dname,Pname;
