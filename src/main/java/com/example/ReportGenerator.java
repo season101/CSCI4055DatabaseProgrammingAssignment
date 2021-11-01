f333ec189310e2eac32506c05ec041b9906c988d
Sijan Malla <sijan.malla101@gmail.com>
2021-10-31 23:48:37 -0500

    Writing to File Works. Merge good for main

--- src/main/java/com/example/ReportGenerator.java
+++ src/main/java/com/example/ReportGenerator.java
@@ -1,27 +1,39 @@
 package com.example;
 
 import java.util.Scanner;
+import java.io.*;
 
 public class ReportGenerator {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Project Executive Summary");
+        File report = new File("ProjectReport.rtf");
+        FileWriter reportWriter = new FileWriter(report);
+
+       // System.out.println("Project Executive Summary");
+        reportWriter.write("Project Executive Summary\n");
         
         Scanner in = new Scanner(ProjectExecutiveSummary.executeProjectExecutiveSummary());
         
         while(in.hasNextLine()){
-            System.out.println(in.nextLine());
+            //System.out.println(in.nextLine());
+            reportWriter.write(in.nextLine()+"\n");
         }
 
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
+        //System.out.println("\n\n\n\n\n\n\n\n\n\n\n");
+        reportWriter.write("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
-        System.out.println("Project Details");
+
+        //System.out.println("Project Details");
+        reportWriter.write("Project Details\n");
+
 
         Scanner ins = new Scanner(ProjectDetails.executeProjectDetails());
         while(ins.hasNextLine()){
-            System.out.println(ins.nextLine());
+            reportWriter.write(ins.nextLine()+"\n");
         }
         in.close();
         ins.close();
+        reportWriter.flush();
+        reportWriter.close();
     }
 }
